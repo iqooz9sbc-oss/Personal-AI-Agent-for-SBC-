c95f4e92efdfc24433c9ce3973c3e70f1716173c
iqooz9sbc-oss <iqooz9sbc@gmail.com>
2026-08-07 01:19:49 +0600

    Update app.py SBC

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to Personal AI Agent for SBC"
+
+if __name__ == "__main__":
+    app.run(debug=True)
